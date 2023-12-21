1bd5a28c86db373439ab12b6590276de576d8abf
EduardoDandolini <eduardodasilvadandolini@hmail.com>
2023-12-20 21:02:23 -0300

    implementado paginação e ordenação e usado anotação  @PageableDefault

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -34,8 +36,8 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam  int pagina, @RequestParam  int quantidade) {
-		Pageable paginacao = PageRequest.of(pagina, quantidade);
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+								 @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
